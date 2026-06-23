a63939e9ecc5a684e67b997f466d7269d87408af
aslam-nikware <aslam.n@ku.th>
2026-06-23 11:30:04 +0700

    first commit

---
+++ JavaLam/JavaProject/src/javaproject/JavaProject.java
@@ -0,0 +1,21 @@
+/*
+ hello world!
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaproject;
+
+/**
+ *
+ * @author Admin
+ */
+public class JavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");//This is Show Hello World
+        System.out.println("สวัดี");
+    }
+}
