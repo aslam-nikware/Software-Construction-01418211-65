05436b2269d87a30b24837baa4d8608fbc934fec
aslam-nikware <aslam.n@ku.th>
2026-07-21 11:47:00 +0700

    add quest 6

---
+++ quest/pkg6/Quest6.java
@@ -0,0 +1,28 @@
+package quest.pkg6;
+
+
+
+public class Quest6 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Byte MAX : " + Byte.MAX_VALUE);
+        System.out.println("Byte MIN : " + Byte.MIN_VALUE);
+        System.out.println("-------------------------");
+
+        
+        System.out.println("Short MAX : " + Short.MAX_VALUE);
+        System.out.println("Short MIN : " + Short.MIN_VALUE);
+        System.out.println("-------------------------");
+
+    
+        System.out.println("Integer MAX : " + Integer.MAX_VALUE);
+        System.out.println("Integer MIN : " + Integer.MIN_VALUE);
+        System.out.println("-------------------------");
+
+       
+        System.out.println("Long MAX : " + Long.MAX_VALUE);
+        System.out.println("Long MIN : " + Long.MIN_VALUE);
+    }
+    
+}
